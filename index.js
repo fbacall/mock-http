fcdfd18ef3fc8e6f149123dc5a02b64de8e9a4da
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-10-10 15:14:11 +0100

    Some basic error checking

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express()
 
 const port = parseInt(process.argv[2] || '3000')
 
-app.get('/:status', function (req, res) {
-    res.status(req.params.status).send('Status: ' + req.params.status)
+app.get('/:status?', function (req, res) {
+    let status = parseInt(req.params.status)
+
+    if (!status || status < 100 || status >= 600) {
+        status = 200
+    }
+
+    res.status(status).send('Status: ' + status)
 })
 
 app.listen(port, function () {
