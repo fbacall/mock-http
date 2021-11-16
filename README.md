6d113724759790e2345ab0e747c143a69960cc59
Finn <finn.bacall@manchester.ac.uk>
2021-11-16 11:34:22 +0000

    Added content length endpoint

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# mock-status-codes
-A webservice to return desired HTTP status codes
+# mock-http
+A webservice to return desired HTTP responses for test purposes
+
+## Endpoints
+`/status/<code>` return given HTTP status code
+
+`/length/<length>` return given `Content-Length` (as `text/plain` with 200 status)
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 const port = parseInt(process.argv[2] || '3000')
 
-app.get('/:status?', function (req, res) {
+app.get('/status/:status?', function (req, res) {
     let status = parseInt(req.params.status)
 
     if (!status || status < 100 || status >= 600) {
@@ -11,6 +11,17 @@ app.get('/:status?', function (req, res) {
     }
 
     res.status(status).send('Status: ' + status)
+})
+
+app.get('/length/:length?', function (req, res) {
+    let length = parseInt(req.params.length)
+
+    res.set({
+        'Content-Type': 'text/plain',
+        'Content-Length': length
+    })
+
+    res.status(200).end()
 })
 
 app.listen(port, function () {
